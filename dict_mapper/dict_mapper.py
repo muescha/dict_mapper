9c67b75e661e1620d739d39106dd4001aa2884f2
muescha <184316+muescha@users.noreply.github.com>
2024-02-02 07:15:04 +0100

    change method order

--- dict_mapper/dict_mapper.py
+++ dict_mapper/dict_mapper.py
@@ -6,6 +6,31 @@ from typing import Dict, Iterable, Any, Union, List, Callable
 #     return True
 
 
+def dict_mapper(
+        data: Union[Dict, Iterable[Dict]],
+        mapper_options: Dict[str, Union[str, Callable]]
+) -> Union[Dict, Iterable[Dict]]:
+    """
+    Map data into a dict or an iterable of dicts
+    :param data:
+    :param mapper_options:
+    :return:
+    """
+    if isinstance(data, Dict):
+        # return dict_mapper_covert(data, mapper_options)
+        # Apply key mapper
+        data = dict_mapper_covert(data, mapper_options)
+        return data
+
+    elif isinstance(data, list):
+        return [
+            dict_mapper(item, mapper_options)
+            for item in data
+        ]
+    else:
+        return data
+
+@DeprecationWarning
 def dict_mapper_covert_recursive(data: Dict, mapper_options: Dict) -> Dict:
     """
     Map data in a dict
@@ -40,6 +65,7 @@ def dict_mapper_covert(data: Dict, mapper_options: Dict) -> Dict:
     print("---------------")
     print(data)
     print("---------------")
+
     # Change Key = f(check(key), key = change(key))
     if 'key_mapper' in mapper_options:
         data = {apply_key_mapper(k, mapper_options['key_mapper']): v for k, v in data.items()}
@@ -53,6 +79,26 @@ def dict_mapper_covert(data: Dict, mapper_options: Dict) -> Dict:
         data = {k: apply_item_mapper(k, v, mapper_options['item_mapper']) for k, v in data.items()}
 
     return data
+
+
+def apply_update_mapper(key: str, value: str, mapper_options: Dict[str, Union[str, Callable]]) -> Any:
+    result = apply_mapper(key, value, mapper_options, lambda f, k, d: f(k, d))
+    print("apply_update_mapper")
+    print(result)
+    if not result == value:
+        return result
+
+
+def apply_item_mapper(key: str, value: str, mapper_options: Dict[str, Union[str, Callable]]) -> Any:
+    return apply_mapper(key, value, mapper_options, lambda f, k, d: f(k, d))
+
+
+def apply_value_mapper(key: str, value: str, mapper_options: Dict[str, Union[str, Callable]]) -> Any:
+    return apply_mapper(key, value, mapper_options, lambda f, k, d: f(d))
+
+
+def apply_key_mapper(key: str, key_mapper: Dict[str, Union[str, List[Callable]]]) -> Any:
+    return apply_mapper(key, key, key_mapper, lambda f, k, d: f(d))
 
 
 def apply_mapper(key: str, value: Any, key_mapper: Dict[str, Union[str, List[Callable]]], wrap: Callable) -> str:
@@ -91,48 +137,3 @@ def apply_mapper(key: str, value: Any, key_mapper: Dict[str, Union[str, List[Cal
                 print(key + ": " + str(value) + " no change - mapper is not type of str/list[callable]/callable ")
                 return value
     return value
-
-
-def apply_update_mapper(key: str, value: str, mapper_options: Dict[str, Union[str, Callable]]) -> Any:
-    result = apply_mapper(key, value, mapper_options, lambda f, k, d: f(k, d))
-    print("apply_update_mapper")
-    print(result)
-    if not result == value:
-        return result
-
-
-def apply_item_mapper(key: str, value: str, mapper_options: Dict[str, Union[str, Callable]]) -> Any:
-    return apply_mapper(key, value, mapper_options, lambda f, k, d: f(k, d))
-
-
-def apply_value_mapper(key: str, value: str, mapper_options: Dict[str, Union[str, Callable]]) -> Any:
-    return apply_mapper(key, value, mapper_options, lambda f, k, d: f(d))
-
-
-def apply_key_mapper(key: str, key_mapper: Dict[str, Union[str, List[Callable]]]) -> Any:
-    return apply_mapper(key, key, key_mapper, lambda f, k, d: f(d))
-
-
-def dict_mapper(
-        data: Union[Dict, Iterable[Dict]],
-        mapper_options: Dict[str, Union[str, Callable]]
-) -> Union[Dict, Iterable[Dict]]:
-    """
-    Map data into a dict or an iterable of dicts
-    :param data:
-    :param mapper_options:
-    :return:
-    """
-    if isinstance(data, Dict):
-        # return dict_mapper_covert(data, mapper_options)
-        # Apply key mapper
-        data = dict_mapper_covert(data, mapper_options)
-        return data
-
-    elif isinstance(data, list):
-        return [
-            dict_mapper(item, mapper_options)
-            for item in data
-        ]
-    else:
-        return data
